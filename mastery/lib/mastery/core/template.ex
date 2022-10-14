e75be7e6b486305e38e586bdd41edb21ae0bf171
Robert Engler <rengler33@gmail.com>
2022-10-13 20:39:10 -0400

    Finish functional core (ch4)

--- mastery/lib/mastery/core/template.ex
+++ mastery/lib/mastery/core/template.ex
@@ -2,4 +2,13 @@ defmodule Mastery.Core.Template do
   # w sigil creates a list of words
   # a modifier creates atoms instead of strings
   defstruct ~w[name category instructions raw compiled generators checker]a
+
+  def new(fields) do
+    raw = Keyword.fetch!(fields, :raw)
+
+    struct!(
+      __MODULE__,
+      Keyword.put(fields, :compiled, EEx.compile_string(raw))
+    )
+  end
 end
